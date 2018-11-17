eccf20ae6709f8f0c7c05d7d9d9be4aa2b8e6ca3
Przemek Czauderna <przemek.czauderna@gmail.com>
2018-11-17 14:59:58 +0100

    add jQuery, sections and new style

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-times");
 
 
-
-
 burger.addEventListener("click", function () {
     burger.classList.toggle("active");
     nav.classList.toggle("showMenu");
-    iconBurger.classList.toggle("active"); //tak
-    iconX.classList.toggle("active"); //nie
+    iconBurger.classList.toggle("active");
+    iconX.classList.toggle("active");
 });
+
+
+// nav jQuery
+$('nav a').on('click', function () {
+    const goToSection = "[data-section=" + $(this).attr('class') + "]";
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    })
+})
